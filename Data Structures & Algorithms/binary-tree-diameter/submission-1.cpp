3b7ae20cfd0e56b0d1b17b8d9396f10d7ad0b793
Maunil Shah <51321172+Maunil2k@users.noreply.github.com>
2026-05-16 11:53:48 +0530

    Add: binary-tree-diameter - submission-1

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-1.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// class Solution {
+// public:
+//     int maxHeight(TreeNode *root) {
+//         if (!root) return 0;
+//         return 1 + max(maxHeight(root->right), maxHeight(root->left));
+//     }
+
+//     int diameterOfBinaryTree(TreeNode* root) {
+//         if (!root) return 0;
+//         int d = maxHeight(root->left) + maxHeight(root->right);
+//         int s = max(
+//             diameterOfBinaryTree(root->left), diameterOfBinaryTree(root->right)
+//         );
+//         return max(d, s);
+//     }
+// };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int res = 0;
+        dfs(root, res);
+        return res;
+    }
+
+private:
+    int dfs(TreeNode* root, int& res) {
+        if (!root) {
+            return 0;
+        }
+        int left = dfs(root->left, res);
+        int right = dfs(root->right, res);
+        res = max(res, left + right);
+        return 1 + max(left, right);
+    }
+};

9d03c3d6e0b8f3d538a5e569389ffa7315245147
Maunil Shah <51321172+Maunil2k@users.noreply.github.com>
2026-05-17 18:23:07 +0530

    Add: level-order-traversal-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-0.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        queue<TreeNode *> q;
+        q.push(root);
+        vector<vector<int>> ans;
+        while (!q.empty()) {
+            int n = q.size();
+            vector<int> tmp;
+            for (int i = 0; i<n; i++) {
+                TreeNode* front = q.front();
+                q.pop();
+                if (front){
+                    tmp.push_back(front->val);
+                    q.push(front->left);
+                    q.push(front->right);
+                }
+            }
+            if (tmp.size()) ans.push_back(tmp);
+        }
+        return ans;
+    }
+};

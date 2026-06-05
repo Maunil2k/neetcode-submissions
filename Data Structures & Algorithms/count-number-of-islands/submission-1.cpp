8d6b98fbedfb85f4e913272fd5e2357d486b0af8
Maunil Shah <51321172+Maunil2k@users.noreply.github.com>
2026-06-05 23:59:51 +0530

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int ans = 0;
+        vector<vector<int>> taken(grid.size(), vector<int>(grid[0].size(), 0));
+        for (int i = 0; i<grid.size(); i++) {
+            for (int j = 0; j<grid[0].size(); j++) {
+                if (grid[i][j] == '1' && !taken[i][j]) {
+                    ans++;
+                    queue<pair<int, int>> bfs;
+                    bfs.push({i,j});
+                    while(!bfs.empty()) {
+                        pair<int, int> tmp = bfs.front();
+                        bfs.pop();
+                        int x = tmp.first, y = tmp.second;
+                        taken[x][y] = 1;
+                        if (x-1>=0 && grid[x-1][y] == '1' && !taken[x-1][y]) bfs.push({x-1, y});
+                        if (y-1>=0 && grid[x][y-1] == '1' && !taken[x][y-1]) bfs.push({x, y-1});
+                        if (x+1<grid.size() && grid[x+1][y] == '1' && !taken[x+1][y]) bfs.push({x+1, y});
+                        if (y+1<grid[0].size() && grid[x][y+1] == '1' && !taken[x][y+1]) bfs.push({x, y+1});
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};

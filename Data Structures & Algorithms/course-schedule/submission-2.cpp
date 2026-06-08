3b5e52b73daa7d4014f9215210a948cd2fdd547f
Maunil Shah <51321172+Maunil2k@users.noreply.github.com>
2026-06-08 23:39:56 +0530

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool dfs(vector<vector<int>> &adjList, vector<int> &visited, int node) {
+        if (visited[node]) return false;
+        visited[node] = 1;
+        for (auto p: adjList[node]) {
+            bool res = dfs(adjList, visited, p);
+            if (!res) return false;
+        }
+        return true;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adjList(numCourses);
+        for (auto &p: prerequisites) {
+            adjList[p[0]].push_back(p[1]);
+        }
+        
+        vector<int> visited(numCourses, 0);
+        for (int i = 0; i<numCourses; i++) {
+            vector<int> dfsVisits(numCourses, 0);
+            bool r = dfs(adjList, dfsVisits, i);
+            if (!r) return false;
+        }
+        return true;
+    }
+};

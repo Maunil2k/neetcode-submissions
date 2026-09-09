d948eef8aa8d06ecd116dfa0174b2208ce5c70bc
Maunil Shah <51321172+Maunil2k@users.noreply.github.com>
2026-09-09 11:51:37 +0530

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> dp(s.size()+1, false);
+        dp[s.size()] = true;
+        for (int i = s.size()-1; i>=0; i--) {
+            for (auto &w: wordDict) {
+                if (i + w.size() <= s.size() && s.substr(i, w.size()) == w) {
+                    if (dp[i+w.size()]) {
+                        dp[i] = dp[i+w.size()];
+                        break;
+                    }
+                }
+            }
+        }
+        
+        return dp[0];
+    }
+};

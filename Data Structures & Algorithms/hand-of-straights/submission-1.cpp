6e9d9f257c3f9ec12af606969ef3cf0c9108b43d
Maunil Shah <51321172+Maunil2k@users.noreply.github.com>
2026-09-10 16:20:39 +0530

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if (hand.size() % groupSize != 0) return false;
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        unordered_map<int, int> counts;
+        for (auto &h: hand) {
+            if (counts.find(h) == counts.end()) {
+                counts[h] = 1;
+                minHeap.push(h);
+            }
+            else counts[h]+=1;
+        }
+
+        int num_groups = hand.size() / groupSize;
+        for (int i = 0; i<num_groups; i++) {
+            int start;
+            for (int j = 0; j<groupSize; j++) {
+                if (j == 0) {
+                    while (1) {
+                        if (counts[minHeap.top()] == 0) minHeap.pop();
+                        else break;
+                    }
+                    start = minHeap.top();
+                    counts[start] -= 1;
+                    if (counts[start] == 0) minHeap.pop();
+                }
+
+                else {
+                    start++;
+                    if (!counts[start]) return false;
+                    counts[start]-=1;
+                }
+            }
+        }
+        return true;
+    }
+};

70d84493f15484975e281c875063f5ff3df84cf8
Maunil Shah <51321172+Maunil2k@users.noreply.github.com>
2026-09-10 11:08:40 +0530

    Add: valid-parenthesis-string - submission-2

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        stack<char> par, star;
+        for (int i = 0; i<s.size(); i++) {
+            if (s[i] == '(') par.push('(');
+            else if (s[i] == ')') {
+                if (!par.empty()) par.pop();
+                else if (!star.empty()) star.pop();
+                else return false;
+            }
+            else star.push('*');
+        }
+
+        while (!par.empty() && !star.empty()) {
+            star.pop();
+            par.pop();
+        }
+
+        return par.empty() ? 1 : 0;
+    }
+};

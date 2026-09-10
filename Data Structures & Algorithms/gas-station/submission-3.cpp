6f33b4b3bdfbad71274594ee25ad96577ffd4bee
Maunil Shah <51321172+Maunil2k@users.noreply.github.com>
2026-09-10 15:40:35 +0530

    Add: gas-station - submission-3

---
+++ Data Structures & Algorithms/gas-station/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int ans=0, n = cost.size(), total = 0;
+        vector<int> diff(n, 0);
+        for (int i = 0; i<n; i++) {
+            diff[i] = gas[i] - cost[i];
+            total += diff[i];
+        }
+        if (total < 0) return -1;
+        total = 0;
+        for (int i = 0; i<n; i++) {
+            total += diff[i];
+            if (total < 0) {
+                ans = i+1;
+                total = 0;
+            }
+        }
+
+        return ans;
+    }
+};

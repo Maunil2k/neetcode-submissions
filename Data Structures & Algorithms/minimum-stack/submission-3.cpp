59f9e83f3fdf550b72afcb7f62fa003759bbd492
Maunil Shah <51321172+Maunil2k@users.noreply.github.com>
2026-09-18 17:08:23 +0530

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.cpp
@@ -0,0 +1,32 @@
+class MinStack {
+private: 
+    stack<int> main_stack;
+    stack<int> min_stack;
+
+public:
+    MinStack() {}
+    
+    void push(int val) {
+        if (main_stack.empty() || val < min_stack.top()) {
+            main_stack.push(val);
+            min_stack.push(val);
+        }
+        else {
+            min_stack.push(min_stack.top());
+            main_stack.push(val);
+        }
+    }
+    
+    void pop() {
+        main_stack.pop();
+        min_stack.pop();
+    }
+    
+    int top() {
+        return main_stack.top();
+    }
+    
+    int getMin() {
+        return min_stack.top();
+    }
+};
